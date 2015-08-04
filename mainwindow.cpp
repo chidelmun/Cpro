7b69d63528c0bffc83cf33498be2d40f9b323cf7
Delveri Munang <chickdelveri@gmail.com>
2015-08-04 13:50:14 +0100

    Cleanup useless variables

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QString safe= "QProgressBar::chunk {background: QLinearGradient( x1: 0, y1: 0, x2: 1, y2: 0,stop: 0 #78d,stop: 0.4999 #46a,stop: 0.5 #45a,stop: 1 #238 );border-bottom-right-radius: 7px;border-bottom-left-radius: 7px;border: 1px solid black;}";
+     ui->progressBar->setStyleSheet(safe);
+    this->setStyleSheet("background-color:rgb(155,68,222)");
     ui->progressBar->reset();
     this->setFixedSize(600,400);
 }
@@ -58,3 +61,5 @@ void MainWindow::on_pushButton_3_clicked()
                 );
     ui->lineEdit->setText(src);
 }
+
+
